011aa28ab8df4fc2bb8f2e59660677bd6522bd9f
piyushversion <piyushdhote190@gmail.com>
2024-03-15 18:18:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import styled,{ThemeProvider} from 'styled-components';
+import {darkTheme,lightTheme} from "./utils/Theme";
+import { Route, Routes } from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+
+const Container = styled.div`
+
+  width:100%;
+  height:100vh;
+  display:flex;
+  background:#15171E;
+  color:#F2F3F4;
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transition: all 0.2s ease;
+`
+const Wrapper = styled.div`
+
+  height:100%;
+  position:relative;
+  display:flex;
+  flex-direction:column;
+  justify-content:space-between;
+  flex:3;
+`
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <ThemeProvider theme={darkTheme}>
+
+        <Container>
+
+          <Wrapper>
+
+            <Navbar></Navbar>
+            <Routes>
+
+              <Route path='/' element={<Home></Home>}></Route>
+              <Route path='/post' element={<CreatePost></CreatePost>}></Route>
+
+            </Routes>
+
+          </Wrapper>
+
+        </Container>
+        
+      </ThemeProvider>
+
   );
 }
 
